979bf45c6aca7434e8ec87f6fea358bad1106328
Mohit Soni <developer_mohitsoni@outlook.com>
2025-02-12 12:56:37 +0530

    Add consumer for subscribed_user_mail_queue to receive messages from RabbitMQ

---
+++ #2 Creating two Queue and it's Consumer/subConsumer.js
@@ -0,0 +1,23 @@
+import amqp from "amqplib";
+
+async function recvMail() {
+  try {
+    // Connect to RabbitMQ
+    const connection = await amqp.connect("amqp://localhost");
+    const channel = await connection.createChannel();
+
+    const queueName = "subscribed_user_mail_queue";
+    await channel.assertQueue(queueName, { durable: false });
+
+    channel.consume(queueName, (message) => {
+      if (message != null) {
+        console.log("Received Message for Sub user :", JSON.parse(message.content));
+        channel.ack(message); // Acknowledge the message to RabbitMQ
+      }
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+recvMail();
