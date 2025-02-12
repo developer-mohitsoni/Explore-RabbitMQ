81976a9ffa7e2a880c655f282b3015b2c52cab23
Mohit Soni <developer_mohitsoni@outlook.com>
2025-02-12 12:56:31 +0530

    Add consumer for users_mail_queue to receive messages from RabbitMQ

---
+++ #2 Creating two Queue and it's Consumer/normalUserConsumer.js
@@ -0,0 +1,26 @@
+import amqp from "amqplib";
+
+async function recvMail() {
+  try {
+    // Connect to RabbitMQ
+    const connection = await amqp.connect("amqp://localhost");
+    const channel = await connection.createChannel();
+
+    const queueName = "users_mail_queue";
+    await channel.assertQueue(queueName, { durable: false });
+
+    channel.consume(queueName, (message) => {
+      if (message != null) {
+        console.log(
+          "Received Message for normal user :",
+          JSON.parse(message.content)
+        );
+        channel.ack(message); // Acknowledge the message to RabbitMQ
+      }
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+recvMail();
