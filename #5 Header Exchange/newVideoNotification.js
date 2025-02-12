be3203828d9b3f3c8186767c3e7fa8e8414036ee
Mohit Soni <developer_mohitsoni@outlook.com>
2025-02-12 16:22:42 +0530

    Add consumer for new video notifications using RabbitMQ headers exchange

---
+++ #5 Header Exchange/newVideoNotification.js
@@ -0,0 +1,46 @@
+import amqp from "amqplib";
+
+const consumeNewVideoNotifications = async () => {
+  try {
+    // Connect to RabbitMQ
+    const connection = await amqp.connect("amqp://localhost");
+    const channel = await connection.createChannel();
+
+    // Setup Exchange
+    const exchange = "headers_exchange";
+    const exchangeType = "headers";
+
+    // Bind the queue to the exchange
+    await channel.assertExchange(exchange, exchangeType, { durable: true });
+
+    // Declare a queue
+
+    // To avoid race condition, we declare the queue as exclusive. This ensures that the queue will be created only once.
+
+    // Note: The queue name is not provided here, RabbitMQ will generate a unique name for each consumer.
+    const q = await channel.assertQueue("", { exclusive: true });
+
+    console.log("Waiting for new video notification ");
+
+    // Bind the queue to the exchange
+    await channel.bindQueue(q.queue, exchange, "", {
+      "x-match": "all", 
+      "notification-type": "new_video",
+      "content-type": "video",
+    });
+
+    channel.consume(q.queue, (msg) => {
+      if (msg !== null) {
+        const message = msg.content.toString();
+        console.log("Received New Video Notification", message);
+
+        // Notification code yahan par ayega!
+        channel.ack(msg);
+      }
+    });
+  } catch (err) {
+    console.error("Error: ", err);
+  }
+};
+
+consumeNewVideoNotifications();
