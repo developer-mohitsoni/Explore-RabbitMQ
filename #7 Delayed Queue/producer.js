ada8cd5998af4117f22da0251d1818082ffbebfd
Mohit Soni <developer_mohitsoni@outlook.com>
2025-04-24 18:50:14 +0530

    refactor: improve formatting and indentation in producer.js

--- #7 Delayed Queue/producer.js
+++ #7 Delayed Queue/producer.js
@@ -1,92 +1,92 @@
 import amqp from "amqplib";
 
 const sendToDelayedQueue = async (batchId, orders, delay) => {
-  try {
-    // Connect to RabbitMQ
-    const connection = await amqp.connect("amqp://localhost");
-    const channel = await connection.createChannel();
+	try {
+		// Connect to RabbitMQ
+		const connection = await amqp.connect("amqp://localhost");
+		const channel = await connection.createChannel();
 
-    // Setup Exchange
-    const exchange = "delayed_exchange";
-    await channel.assertExchange(exchange, "x-delayed-message", {
-      arguments: {
-        "x-delayed-type": "direct",
-      },
-    });
+		// Setup Exchange
+		const exchange = "delayed_exchange";
+		await channel.assertExchange(exchange, "x-delayed-message", {
+			arguments: {
+				"x-delayed-type": "direct",
+			},
+		});
 
-    const queue = "delayed_order_updates_queue";
+		const queue = "delayed_order_updates_queue";
 
-    await channel.assertQueue(queue, {
-      durable: true,
-    });
+		await channel.assertQueue(queue, {
+			durable: true,
+		});
 
-    await channel.bindQueue(queue, exchange, "");
+		await channel.bindQueue(queue, exchange, "");
 
-    const message = JSON.stringify({ batchId, orders });
-    channel.publish(exchange, "", Buffer.from(message), {
-      headers: {
-        "x-delay": delay, // Set the delay for the message
-      },
-    });
+		const message = JSON.stringify({ batchId, orders });
+		channel.publish(exchange, "", Buffer.from(message), {
+			headers: {
+				"x-delay": delay, // Set the delay for the message
+			},
+		});
 
-    console.log("---------------------------- \n");
-    console.log(
-      `Sent batch ${batchId} update task to delayed queue with ${delay} ms delay `
-    );
-    console.log("---------------------------- \n");
+		console.log("---------------------------- \n");
+		console.log(
+			`Sent batch ${batchId} update task to delayed queue with ${delay} ms delay `,
+		);
+		console.log("---------------------------- \n");
 
-    await channel.close();
-    await connection.close();
-  } catch (err) {
-    console.error("Error while sending mail:", err);
-  }
+		await channel.close();
+		await connection.close();
+	} catch (err) {
+		console.error("Error while sending mail:", err);
+	}
 };
 
-const processBatchOrders = async () => {
-  // Simulate batch processing
-  const batchId = generateBatchId();
-  const orders = collectOrdersForBatch();
+const processBatchOrders = () => {
+	// Simulate batch processing
+	const batchId = generateBatchId();
+	const orders = collectOrdersForBatch();
 
-  console.log("---------------------------- \n");
-  console.log(
-    `Processing batch ${batchId} with orders ${JSON.stringify(orders)})}`
-  );
-  console.log("---------------------------- \n");
+	console.log("---------------------------- \n");
+	console.log(
+		`Processing batch ${batchId} with orders ${JSON.stringify(orders)})}`,
+	);
+	console.log("---------------------------- \n");
 
-  // Update inventory, generate shipping labels, etc.
-  await processOrders(orders);
+	// Update inventory, generate shipping labels, etc.
+	processOrders(orders);
 
-  // Send delayed message to update order status
-  const delay = 10000; // 10 sec
-  sendToDelayedQueue(batchId, orders, delay);
+	// Send delayed message to update order status
+	const delay = 10000; // 10 sec
+	sendToDelayedQueue(batchId, orders, delay);
 };
 
 const generateBatchId = () => {
-  return "batch- " + Date.now();
+	return `batch- ${Date.now()}`;
 };
 
 const collectOrdersForBatch = () => {
-  // Collect orders for the current batch
+	// Collect orders for the current batch
 
-  return [
-    {
-      orderId: 1,
-      item: "Laptop",
-      quantity: 1,
-    },
-    {
-      orderId: 2,
-      item: "Phone",
-      quantity: 2,
-    },
-  ];
+	return [
+		{
+			orderId: 1,
+			item: "Laptop",
+			quantity: 1,
+		},
+		{
+			orderId: 2,
+			item: "Phone",
+			quantity: 2,
+		},
+	];
 };
 
 const processOrders = (orders) => {
-  // Process orders
-  // Update inventory, generate shipping labels, etc.
-  // For demonstration purposes, just log the orders
-  console.log("Processing orders:", JSON.stringify(orders));
+	// Process orders
+	// Update inventory, generate shipping labels, etc.
+	// For demonstration purposes, just log the orders
+	console.log("Processing orders:", JSON.stringify(orders));
 };
 
 processBatchOrders();
