b6dc263176d02ea911b92424274261f65f01bc6a
Mohit Soni <developer_mohitsoni@outlook.com>
2025-04-24 15:50:15 +0530

    refactor: improve formatting and indentation in consumer.js

--- #1 Getting Start with RabbitMQ/consumer.js
+++ #1 Getting Start with RabbitMQ/consumer.js
@@ -1,23 +1,23 @@
 import amqp from "amqplib";
 
 async function recvMail() {
-  try {
-    // Connect to RabbitMQ
-    const connection = await amqp.connect("amqp://localhost");
-    const channel = await connection.createChannel();
+	try {
+		// Connect to RabbitMQ
+		const connection = await amqp.connect("amqp://localhost");
+		const channel = await connection.createChannel();
 
-    const queueName = "mail_queue";
-    await channel.assertQueue(queueName, { durable: false });
+		const queueName = "mail_queue";
+		await channel.assertQueue(queueName, { durable: false });
 
-    channel.consume(queueName, (message) => {
-      if (message != null) {
-        console.log("Received Message :", JSON.parse(message.content));
-        channel.ack(message); // Acknowledge the message to RabbitMQ
-      }
-    });
-  } catch (err) {
-    console.error(err);
-  }
+		channel.consume(queueName, (message) => {
+			if (message != null) {
+				console.log("Received Message :", JSON.parse(message.content));
+				channel.ack(message); // Acknowledge the message to RabbitMQ
+			}
+		});
+	} catch (err) {
+		console.error(err);
+	}
 }
 
 recvMail();
